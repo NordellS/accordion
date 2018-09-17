aae99159f396b6935f760b3b532f8abb7a2c65c3
SaraNordell <sara@nordell.me>
2018-09-17 14:44:06 +0200

    icon collapsed + expand added

--- code/script.js
+++ code/script.js
@@ -1,10 +1,12 @@
 // A function that adds and remove the class "active" on the section you click on.
 function toggleClass() {
   this.classList.toggle("active")
-}
+  }
+
 
 // Selects and HTML element, and calls a function which will be executed when the element is clicked.
 document.getElementById("section1").onclick = toggleClass
 document.getElementById("section2").onclick = toggleClass
 document.getElementById("section3").onclick = toggleClass
- 
+
+// // text icon - expand collaps

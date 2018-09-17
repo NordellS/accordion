045464ec592031f11363eb82a1a01d66260c3072
SaraNordell <sara@nordell.me>
2018-09-17 13:48:45 +0200

    Assignment accordion step 1-4

--- code/script.js
+++ code/script.js
@@ -1,7 +1,10 @@
 // A function that adds and remove the class "active" on the section you click on.
-function toggle() {
+function toggleClass() {
   this.classList.toggle("active")
 }
 
 // Selects and HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+document.getElementById("section1").onclick = toggleClass
+document.getElementById("section2").onclick = toggleClass
+document.getElementById("section3").onclick = toggleClass
+ 
